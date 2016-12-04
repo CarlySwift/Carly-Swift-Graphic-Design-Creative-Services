a8879b319c9c8d7868db0a5dede5f79d6a17f631
CarlySwift <carlyswifta3@gmail.com>
2016-12-04 10:46:09 -0800

    working on hidden sections and associated animations

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
+//Animating the header content.
+
 $(function(){
 
     $("#banner").hide().delay(500).slideDown(500).fadeIn('fast');
 
 });
 
+$(function(){
+
+    $("#banner2").hide().delay(800).slideDown(800).fadeIn('fast');
+
+});
+
+
+//Getting the page to auto scroll to the form when the header button is clicked
 
 $(function() {
   // Generic selector to be used anywhere
@@ -18,3 +28,16 @@ $(function() {
     }, 500);
   });
 });
+
+
+// Getting the 3 "solutions buttons to reveal their content on click
+
+  $("show-print").hide();
+
+  
+$(function(){
+
+  $(".print").click(function(){
+    $("show-print").show();
+});
+})
